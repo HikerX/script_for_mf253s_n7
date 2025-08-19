a511e14517057d7a017bc4b81be975d481ae144e
HikerX <yinlaile@live.com>
2025-08-20 02:20:24 +0800

    update

--- handle_cpe_sms.sh
+++ handle_cpe_sms.sh
@@ -24,13 +24,11 @@ POST_DATA='{
     "password": "YWRtaW4="
 }'
 
-#Bark通知服务Key env export BarkKey
-Bark_Key="$BarkKey"
 
 #查询短信结果，全局变量
 hasLogin="false"
 msgArrRst=''
-runCount=0;
+shoulShowCapacity="true";
 
 
 # ==============================================
@@ -96,13 +94,12 @@ function decode_message() {
 
 #bark
 function notify_bark(){
-    [[ -z "$Bark_Key" ]] && return
     echo -e "\n发送通知 $1"
     response=$(curl -X "POST"\
          -d "title=$1" \
          -d "body=$2" \
          --silent \
-         "https://api.day.app/$Bark_Key")
+         "https://api.day.app/$BARK_KEY")
     # 解析响应结果
     #http_status=$(echo "$response" | tr -d '\n' | sed -e 's/.*HTTP_STATUS://')
     body=$(echo "$response" | sed -e 's/HTTP_STATUS:.*//')
@@ -111,11 +108,11 @@ function notify_bark(){
     #echo "Response Body:"
     #echo "$body" | jq . 2>/dev/null || echo "$body"
     #{"code":200,"message":"success","timestamp":1755502279}
-    if [[ "$body" =~ success ]]; then echo "成功"; else echo "失败";fi
+    ! [[ "$body" =~ success ]] && echo "$body";
 }
 
 
-#service getLoginStatus
+#service.getLoginStatus
 function getLoginStatus(){
     #echo -e "\n登录状态"
     response=$(curl \
@@ -220,7 +217,7 @@ function getSMSMessages(){
 #msgIds "id1;id2;...idn;" 特别要求结尾分号不能少
 function setSmsRead(){
     # 执行curl命令
-    echo -e "\n设为已读 id=$1"
+    #echo -e "\n设为已读 id=$1"
     response=$(curl -X "POST" \
       -H "${HEADERS[0]}" \
       -H "${HEADERS[1]}" \
@@ -243,7 +240,7 @@ function setSmsRead(){
     #echo "HTTP Status: $http_status"
     #echo "Response Body:"
     #echo "$body" | jq . 2>/dev/null || echo "$body"
-    if [[ "$body" =~ success ]]; then echo "操作成功"; else echo "操作失败"; fi
+    ! [[ "$body" =~ success ]] && echo "$body";
 }
 
 :<<'comment'
@@ -284,9 +281,8 @@ function getSmsCapability(){
     done < <(echo "$body" | jq -r '. | to_entries[] | "\(.key)=\(.value)"')
     used=$(( ${dict["sms_nv_rev_total"]}+${dict["sms_nv_send_total"]}+${dict["sms_nv_draftbox_total"]} ))
     max="${dict['sms_nv_total']}"
-
-    if [ "$runCount" == 0 ]; then echo "容量 $used / $max"; fi
-    if [ $(($used + 10 )) -le "$max" ]; then return; fi
+    if [ "$shoulShowCapacity" == "true" ]; then echo "容量 $used / $max"; fi
+    [ $(($used + 10 )) -le "$max" ] && shoulShowCapacity="false"; return;
     toDelIds=""
     delQty=$(($used - 10 )) #keep 10 left
     while read -r line; do
@@ -323,7 +319,8 @@ function deleteMessage(){
     #echo "HTTP Status: $http_status"
     #echo "Response Body:"
     #echo "$body" | jq . 2>/dev/null || echo "$body"
-    if [[ "$body" =~ success ]]; then echo "操作成功"; else echo "操作失败"; fi
+    if [[ "$body" =~ success ]]; then echo "删除成功"; else echo "$body"; fi
+    shoulShowCapacity="true"
 }
 
 :<<'comment'
@@ -343,7 +340,7 @@ function lookForUnreaded(){
       dict[$k]=$v
     done < <(echo $msgArrRst | jq -r ' .messages | map (select(.tag=="1")) | if length > 0 then last else {} end | to_entries[] | "\(.key)=\(.value)"')
     #${#array[@]} 返回元素数量, 获取关联数组元素数量的标准方法，与普通数组的用法一致
-    if [ "${#dict[@]}" -eq 0 ]; then return 0; fi
+    [ "${#dict[@]}" -eq 0 ] && return;
     #echo ${dict["tag"]}
     #echo ${dict["id"]}
     #echo ${dict["date"]}
@@ -353,20 +350,21 @@ function lookForUnreaded(){
     plainContent=$(decode_message ${dict["content"]})
     echo "${dict['number']}"
     echo "$plainContent"$'\n'"$msgDate"  # $'\n' 动态换行 直接\n换行没效果
+    shoulShowCapacity="true"
     if [[ "$plainContent" =~ 验证密?码|流量使用提醒 ]]; then
         notify_bark  "${dict['number']}"  "$plainContent"$'\n'"$msgDate";
         deleteMessage "${dict['id']};"
     else
-
         setSmsRead "${dict['id']};"
     fi
 }
 
-
 function init(){
     getLoginStatus
-    if [ "$hasLogin" == "true" ]; then getSmsCapability; getSMSMessages; ((runCount++)); else login; fi
-    sleep 15
+    if [ "$hasLogin" == "true" ]; then getSmsCapability; getSMSMessages; else login; fi
+
+    sleep 45
     init
 }
-if [[ -z "$Bark_Key" ]]; then echo "BarkKey未设置"; else init; fi
+#Bark通知服务Key, set env: export BARK_KEY="..."
+if [[ -z "$BARK_KEY" ]]; then echo "未设置环境变量 BARK_KEY"; else init; fi;
