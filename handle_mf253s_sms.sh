1fc1580529551d45d9199952b2639fa141996e1c
HikerX <yinlaile@live.com>
2025-08-20 10:10:12 +0800

    移除 使用 关联数组，进程交换

--- handle_mf253s_sms.sh
+++ handle_mf253s_sms.sh
@@ -14,6 +14,7 @@ HEADERS=(
   "Accept:  application/json, text/javascript, */*; q=0.01"
   "Content-Type: application/x-www-form-urlencoded; charset=UTF-8"
   "Referer: http://192.168.0.1/index.html"
+  "Content-Type: application/json; charset=UTF-8"
 )
 
 # POST数据内容 (JSON格式)
@@ -27,8 +28,7 @@ POST_DATA='{
 
 #查询短信结果，全局变量
 hasLogin="false"
-msgArrRst=''
-shoulShowCapacity="true";
+msgArrStr=''
 
 
 # ==============================================
@@ -92,14 +92,24 @@ function decode_message() {
 }
 
 
-#bark
+#bark if [[ "$plainContent" =~ 验证密?码|流量使用提醒 ]];
 function notify_bark(){
     echo -e "\n发送通知 $1"
-    response=$(curl -X "POST"\
-         -d "title=$1" \
-         -d "body=$2" \
+    # 构建JSON负载, 把device_key放入请求体，而非url
+    # sound tweet 鸟鸣; telegraph 电报; calypso 卡利普索; horn 号角; chime 铃声; tiptoes 踮脚尖
+    payload=$(jq -n \
+        --arg title "$1" \
+        --arg body "$2" \
+        --arg isArchive "$3" \
+        --arg sound "calypso" \
+        --arg device_key "$BARK_KEY" \
+        '{title: $title, body: $body, device_key: $device_key, isArchive: $isArchive, sound: $sound}' )
+
+    response=$(curl  -X "POST"\
+         -H "Content-Type: application/json; charset=UTF-8" \
+         -d "$payload" \
          --silent \
-         "https://api.day.app/$BARK_KEY")
+         "https://api.day.app/push")
     # 解析响应结果
     #http_status=$(echo "$response" | tr -d '\n' | sed -e 's/.*HTTP_STATUS://')
     body=$(echo "$response" | sed -e 's/HTTP_STATUS:.*//')
@@ -118,9 +128,10 @@ function getLoginStatus(){
     response=$(curl \
       -H "${HEADERS[0]}" \
       -H "${HEADERS[2]}" \
-      -d isTest=false \
-      -d cmd=loginfo \
-      -d multi_data=1 \
+      -d "isTest=false" \
+      -d "cmd=loginfo" \
+      -d "multi_data=1" \
+      -d "_=$(date +%s%3N)" \
       --silent \
       "${API_GET_URL}")
 
@@ -150,6 +161,7 @@ function login(){
       -d "isTest=false" \
       -d "goformId=LOGIN" \
       -d "password=$(echo -n "admin" | base64)" \
+      -d "_=$(date +%s%3N)" \
       --silent \
       "$API_SET_URL" )
 
@@ -187,14 +199,14 @@ function getSMSMessages(){
     response=$(curl \
       -H "${HEADERS[0]}" \
       -H "${HEADERS[2]}" \
-      -d isTest=false \
-      -d cmd=sms_data_total \
-      -d page=0 \
-      -d data_per_page=500 \
-      -d mem_store=1 \
-      -d tags=10 \
-      -d order_by="order by id desc" \
-      -d _=$(date +%s%3N) \
+      -d "isTest=false" \
+      -d "cmd=sms_data_total" \
+      -d "page=0" \
+      -d "data_per_page=500" \
+      -d "mem_store=1" \
+      -d "tags=10" \
+      -d "order_by=order by id desc" \
+      -d "_=$(date +%s%3N)" \
       --silent \
       "${API_GET_URL}")
 
@@ -209,7 +221,11 @@ function getSMSMessages(){
     # =~ 字符正则表达式匹配符号，这里的正则表达式更简化了
     # {messages: [{"id":"15145","content":"30109A8C8B","tag":"0","date": "25,08,15,22,23,56,+32","draft_group_id":""}]}'
     #{"sms_data_total":""}
-    if [[ "$body" =~ number ]]; then msgArrRst="$body"; lookForUnread; else echo "$body"; fi
+    if [[ "$body" =~ messages ]]; then 
+        msgArrStr=$(echo "$body" | jq -r '.messages')
+        msgArrLen=$(echo "$msgArrStr" | jq -r '. |  length') 
+        [ "$msgArrLen" -gt 0 ] && lookForUnread; 
+    else echo "$body"; fi
 }
 
 
@@ -226,6 +242,7 @@ function setSmsRead(){
       -d "goformId=SET_MSG_READ" \
       -d "msg_id=$1" \
       -d "tag=0" \
+      -d "_=$(date +%s%3N)" \
       --silent \
       "$API_SET_URL" )
 
@@ -257,13 +274,13 @@ function setSmsRead(){
 comment
 #service.getSmsCapability
 function getSmsCapability(){
-    echo -e "\n查询容量"
+    #echo -e "\n查询容量"
     response=$(curl \
       -H "${HEADERS[0]}" \
       -H "${HEADERS[2]}" \
       -d isTest=false \
       -d cmd=sms_capacity_info \
-      -d _=$(date +%s%3N) \
+      -d "_=$(date +%s%3N)" \
       --silent \
       "${API_GET_URL}")
 
@@ -275,28 +292,17 @@ function getSmsCapability(){
     #echo "HTTP Status: $http_status"
     #echo "Response Body:"
     #echo "$body" | jq . 2>/dev/null || echo "$body"
-    echo "Test point A"
-    declare -A dict
-    while IFS="=" read -r k v; do
-      dict[$k]=$v
-    #done < <(...) 进程替换
-    done < <(echo "$body" | jq -r '. | to_entries[] | "\(.key)=\(.value)"')
-    #${#array[@]} 返回元素数量, 获取关联数组元素数量的标准方法，与普通数组的用法一致
-    echo "Test point B"
-    [ "${#dict[@]}" -eq 0 ] && return;
-    echo "Test point C"
-    sms_total="${dict["sms_nv_total"]}"
-    echo "$sms_total"
-    used=$(( "${dict['sms_nv_rev_total']}" + "${dict['sms_nv_send_total']}" + "${dict['sms_nv_draftbox_total']}" ))
-    max="${dict['sms_nv_total']}"
-    if [ "$shoulShowCapacity" == "true" ]; then echo "容量 $used / $max"; fi
-    [ $(($used + 10 )) -le "$max" ] && shoulShowCapacity="false"; return;
-    toDelIds=""
-    delQty=$(($used - 10 )) #keep 10 left
-    while read -r line; do
-        toDelIds+="$line;"
-    done < <(echo $msgArrRst | jq -r " .messages | .[-$delQty: ] | .[] | .id")
-
+    #declare -A dict; #使用 关联数组 和 进程替换 ，在 termux (an)测试通过，在iSH(iOS)测试失败
+    sms_nv_size=$(echo "$body" | jq -r '.sms_nv_total')
+    sms_nv_rev_qty=$(echo "$body" | jq -r '.sms_nv_rev_total')
+    sms_nv_send_qty=$(echo "$body" | jq -r '.sms_nv_send_total')
+    sms_nv_draftbox_qty=$(echo "$body" | jq -r '.sms_nv_draftbox_total')    
+    used=$(( "$sms_nv_rev_qty" + "$sms_nv_send_qty" + "$sms_nv_draftbox_qty" ))
+    [ $(($used + 10)) -le "$sms_nv_size" ] && return;
+    delQty=$(( "$used" - 10 )) #keep 10 left
+    echo "容量已用 $used / $sms_nv_size, 删除最早 $delQty 条, $(date +'%Y-%m-%d %H:%M:%S')"
+    toDelIds=$(echo "$msgArrStr" | jq -r ".[-$delQty: ] | [.[].id] | join(\";\")")    
+    #echo "toDelIds: $toDelIds"
     deleteMessage "$toDelIds"
 }
 
@@ -313,6 +319,7 @@ function deleteMessage(){
       -d "goformId=DELETE_SMS" \
       -d "msg_id=$1" \
       -d "notCallback=true" \
+      -d "_=$(date +%s%3N)" \
       --silent \
       "$API_SET_URL" )
 
@@ -328,7 +335,6 @@ function deleteMessage(){
     #echo "Response Body:"
     #echo "$body" | jq . 2>/dev/null || echo "$body"
     if [[ "$body" =~ success ]]; then echo "删除成功 $1"; else echo "$body"; fi
-    shoulShowCapacity="true"
 }
 
 :<<'comment'
@@ -343,38 +349,37 @@ function deleteMessage(){
 comment
 function lookForUnread(){
     #echo -e "\n寻找未读"
-     echo "Test point A"
-    declare -A dict
-    while IFS="=" read -r k v; do
-      dict[$k]=$v
-    done < <(echo $msgArrRst | jq -r ' .messages | map (select(.tag=="1")) | if length > 0 then last else {} end | to_entries[] | "\(.key)=\(.value)"')
-    #${#array[@]} 返回元素数量, 获取关联数组元素数量的标准方法，与普通数组的用法一致
-     echo "Test point B"
-    [ "${#dict[@]}" -eq 0 ] && return;
-     echo "Test point C"
-    #echo ${dict["tag"]}
-    #echo ${dict["id"]}
-    #echo ${dict["date"]}
-    # "25,08,17,23,40,33,+32" -> "08-17 23:40:33"
-    msgDate=$(echo ${dict["date"]} | sed 's/\([0-9]\+\),\([0-9]\+\),\([0-9]\+\),\([0-9]\+\),\([0-9]\+\),\([0-9]\+\),.*/\2-\3 \4:\5:\6/g')
-    #echo ${dict["content"]}
-    plainContent=$(decode_message ${dict["content"]})
-    echo "${dict['number']}"
-    echo "$plainContent"$'\n'"$msgDate"  # $'\n' 动态换行 直接\n换行没效果
-    shoulShowCapacity="true"
-    if [[ "$plainContent" =~ 验证密?码|流量使用提醒 ]]; then
-        notify_bark  "${dict['number']}"  "$plainContent"$'\n'"$msgDate";
-        deleteMessage "${dict['id']};"
+    msg_unread=$(echo "$msgArrStr" | jq -r ' map(select(.tag=="1")) | if length > 0 then last else "" end')
+    #echo "msg_unread: $msg_unread"
+    [[ -z $msg_unread ]] && return;
+    msg_id=$(echo "$msg_unread" | jq -r '.id')
+    msg_number=$(echo "$msg_unread" | jq -r '.number')
+    msg_content=$(echo "$msg_unread" | jq -r '.content')
+    msg_tag=$(echo "$msg_unread" | jq -r '.tag')
+    msg_date=$(echo "$msg_unread" | jq -r '.date')
+    msg_draft_group_id=$(echo "$msg_unread" | jq -r '.draft_group_id')
+
+    #echo ${msg_id}
+    #echo ${msg_tag}
+    #echo ${msg_date}
+    #echo ${msg_content}
+    # "25,08,17,23,40,33,+32" -> "2025-08-17 23:40:33"
+    msgDate=$(echo "$msg_date" | sed "s/\([0-9]\+\),\([0-9]\+\),\([0-9]\+\),\([0-9]\+\),\([0-9]\+\),\([0-9]\+\),.*/$(date +%Y)-\2-\3 \4:\5:\6/g")
+    plainContent=$(decode_message "$msg_content")
+    echo "$msg_number: $plainContent"$'\n'"$msgDate"  # $'\n' 动态换行 直接\n换行没效果
+    if [[ "$plainContent" =~ 验证密?码|流量使用提醒|流量用尽提醒 ]]; then
+        isArchive="1" #bark是否存档, 验证码不存档
+        [[ "$plainContent" =~ 验证密?码 ]] && isArchive="0" && deleteMessage "$msg_id;"
+        notify_bark  "$msg_number"  "$plainContent"$'\n'"$msgDate"  "$isArchive";
     else
-        setSmsRead "${dict['id']};"
+        setSmsRead "$msg_id;"
     fi
 }
 
 function init(){
     getLoginStatus
-    if [ "$hasLogin" == "true" ]; then getSMSMessages; else login; fi
-    #getSmsCapability; 
-
+    #优先获取短信列表，再查询容量;假设当前容量已满，需要知道短信id，才能进行删除操作，
+    if [ "$hasLogin" == "true" ]; then getSMSMessages; getSmsCapability; else login; fi
     sleep 15
     init
 }
