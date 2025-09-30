c460a31018dfe0e8f25c0c7df8c393dac985ea42
hiker <yong.wen@live.com>
2025-09-30 22:43:45 +0800

    Update Baidu-Direct-New.lua

--- Baidu-Direct-New.lua
+++ Baidu-Direct-New.lua
@@ -44,7 +44,7 @@ function wa_lua_on_handshake_cb(ctx)
     if flags[uuid] ~= kHttpHeaderSent then
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
-        local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
+        local res = 'CONNECT ' .. host .. ':' .. port .. 'HTTP/1.1\r\n' ..
                     'Host:mbh.chinaunicomvideo.cn\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'X-T5-Auth: 683556433\r\nUser-Agent: baiduboxapp\r\n\r\n'
